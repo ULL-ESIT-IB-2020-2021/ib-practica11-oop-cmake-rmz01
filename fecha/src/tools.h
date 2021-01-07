c11a7559df422da06fa059bce83ba8a93ae530e7
Aarón <72323217+rmz01@users.noreply.github.com>
2021-01-07 12:46:27 +0000

    Create tools.h

---
+++ fecha/src/tools.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+const std::string kHelpText = "Incluya un método que permita determinar si el año correspondiente a una fecha es un año bisiesto o no.
+Realice un programa cliente fechas.cc que tome como parámetro una fecha, un número y un nombre de fichero: ";
+
+void Usage(int argc, char *argv[]);
